d19499623e07a3adae68ef4a9b7dfec9ae05bb74
VanshSh <vanshsharma9354@gmail.com>
2022-05-15 12:39:59 +0530

    docs: :memo: Update the Data
    Changed the Community data and added the edit option

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const communityData = [
+    {
+        id: 1,
+        name: 'Theresa Webb',
+        emoji: '👋',
+        img: 'https://randomuser.me/api/portraits/women/8.jpg',
+        msg: 'Lorem ipsum dolor sit amet consectetur adipisicing m nesectesectetur adipisicing m netur adipisicing sectetursectetur adipisicing m ne adipisicing m nem nesciunt.',
+        time: '5 mins ago',
+        comments: 24,
+        edited: false,
+    },
+    {
+        id: 2,
+        name: 'Marvin McKinney',
+        emoji: '😔',
+        img: 'https://randomuser.me/api/portraits/men/86.jpg',
+        msg: 'Lorem ipsum dolor sit amet c cusectetur adipisicing m nesectetur adipisicing m nelsectetur adipisicing m nsectetur adipisicing m neepa veniam nesciunt.',
+        time: '8 mins ago',
+        comments: 20,
+        edited: true,
+    },
+]
